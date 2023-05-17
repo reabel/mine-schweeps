695b5d836d2588e6550ed6dce48148377d530a7d
reabel <ryanreabel@gmail.com>
2023-05-16 20:16:52 -0400

    intial commit of the main python file

---
+++ main.py
@@ -0,0 +1,83 @@
+import random
+import os
+
+def create_board(rows, cols, bombs):
+    # Create an empty board
+    board = [['-' for j in range(cols)] for i in range(rows)]
+    # Place the bombs randomly
+    for i in range(bombs):
+        row, col = random.randint(0, rows - 1), random.randint(0, cols - 1)
+        while board[row][col] == '*':
+            row, col = random.randint(0, rows - 1), random.randint(0, cols - 1)
+        board[row][col] = '*'
+    # Calculate the number of bombs in each cell
+    for i in range(rows):
+        for j in range(cols):
+            if board[i][j] != '*':
+                bombs_count = 0
+                for x in range(max(0, i - 1), min(i + 2, rows)):
+                    for y in range(max(0, j - 1), min(j + 2, cols)):
+                        if board[x][y] == '*':
+                            bombs_count += 1
+                board[i][j] = str(bombs_count)
+    return board
+
+def display_board(board):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for row in board:
+        print(' '.join(row))
+
+def play():
+    rows, cols, bombs = 10, 10, 10
+    board = create_board(rows, cols, bombs)
+    display_board(board)
+    while True:
+        row, col = map(int, input('Enter row and column: ').split())
+        if board[row][col] == '*':
+            print('Game over!')
+            display_board(board)
+            break
+        elif board[row][col] != '-':
+            print('Cell already revealed!')
+        else:
+            reveal_cells(board, row, col)
+            display_board(board)
+            if check_win(board):
+                print('You won!')
+                break
+
+def reveal_cells(board, row, col):
+    if board[row][col] != '-':
+        return
+    board[row][col] = ' '
+    for x in range(max(0, row - 1), min(row + 2, len(board))):
+        for y in range(max(0, col - 1), min(col + 2, len(board[0]))):
+            if board[x][y] == '-':
+                reveal_cells(board, x, y)
+
+def check_win(board):
+    for row in board:
+        if '-' in row:
+            return False
+    return True
+
+# def get_user_input():
+#     while True:
+#         try:
+#             row, col = map(int, input('Enter row and column: ').split())
+#             if row < 0 or row >= rows or col < 0 or col >= cols:
+#                 raise ValueError()
+#             return row, col
+#         except (ValueError, IndexError):
+#             print('Invalid input! Please enter valid row and column values.')
+
+# def play():
+#     # Existing code...
+#     while True:
+#         row, col = get_user_input()
+#         # Existing code...
+
+
+
+if __name__ == '__main__':
+    play()
